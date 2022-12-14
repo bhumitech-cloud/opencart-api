a37e23317c55416a3360a0a375b1be3f5c545322
bhumitech-cloud <bhumitmalvi999@gmail.com>
2022-12-14 17:59:37 +0530

    added login check

--- catalog/controller/api/register.php
+++ catalog/controller/api/register.php
@@ -23,11 +23,11 @@ class ControllerApiRegister extends Controller
 			$json['customer_id'] = $this->model_account_customer->addCustomer($this->request->post);
 
 			// Clear any previous login attempts for unregistered accounts.
-			//$this->model_account_customer->deleteLoginAttempts($this->request->post['email']);
+			$this->model_account_customer->deleteLoginAttempts($this->request->post['email']);
 
-			//$this->customer->login($this->request->post['email'], $this->request->post['password']);
+			$this->customer->login($this->request->post['email'], $this->request->post['password']);
 
-			//unset($this->session->data['guest']);
+			unset($this->session->data['guest']);
 			
 
             $json['status'] = true;
@@ -38,7 +38,7 @@ class ControllerApiRegister extends Controller
 		if (!isset($json['customer_id'])){
 			$json['status'] = false;
 			$this->response->addHeader('Content-Type: application/json');
-        $this->response->setOutput(json_encode($json));
+        	$this->response->setOutput(json_encode($json));
 		}
         
 			//$this->response->redirect($this->url->link('account/success'));
